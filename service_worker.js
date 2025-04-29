ec133a95589cb2508cacb1dce4de248fb5dae561
iamvalenciia <val22018@byui.edu>
2025-04-29 06:59:02 -0500

    deploying project structure

---
+++ service_worker.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed");
+  chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: showPopup,
+    });
+  });
+});
